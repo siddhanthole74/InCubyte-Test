6faaab891267ca995c2a4d953b0b64bd44d43af0
siddhanthole74 <siddhanthole74@gmail.com>
2023-09-15 14:20:42 +0530

    Commit 6 : Testing of Code

--- Solution.cpp
+++ Solution.cpp
@@ -9,114 +9,124 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
 class LunarCraft {
-    private:
+private:
     int positionX;
     int positionY;
     int positionZ;
     char direction;
+
 public:
-    LunarCraft(int x, int y, int z, char initialDirection) {
+    // parameterized Constructor
+    LunarCraft(int x, int y, int z, char initialDirection)
+    {
         positionX = x;
         positionY = y;
         positionZ = z;
         direction = initialDirection;
     }
-
+    // implementation of moveforword function
     void moveForward() 
     {
-        if (direction == 'N') {
+        if (direction == 'N') 
+        {
             positionY++;
         } 
-        else if (direction == 'S') {
+        else if (direction == 'S') 
+        {
             positionY--;
         } 
-        else if (direction == 'E') {
+        else if (direction == 'E') 
+        {
             positionX++;
         } 
-        else if (direction == 'W') {
+        else if (direction == 'W') 
+        {
             positionX--;
         } 
-        else if (direction == 'U') {
+        else if (direction == 'U')
+        {
             positionZ++;
         } 
-        else if (direction == 'D') {
+        else if (direction == 'D') 
+        {
             positionZ--;
         }
     }
 
+    // implementation of movebackword function
     void moveBackward() 
     {
-        // Implement backward movement similarly to forward movement
         if (direction == 'N') 
         {
-        positionY--; // Reverse the Y coordinate change
+            positionY--;
         } 
         else if (direction == 'S') 
         {
-        positionY++; // Reverse the Y coordinate change
+            positionY++;
         } 
         else if (direction == 'E') 
         {
-        positionX--; // Reverse the X coordinate change
+            positionX--;
         } 
         else if (direction == 'W') 
         {
-        positionX++; // Reverse the X coordinate change
+            positionX++;
         } 
         else if (direction == 'U') 
         {
-        positionZ--; // Reverse the Z coordinate change
+            positionZ--;
         } 
         else if (direction == 'D') 
         {
-        positionZ++; // Reverse the Z coordinate change
+            positionZ++;
         }
     }
 
+    // implementation of rotate left function
     void rotateLeft() 
     {
-        // Implement left rotation
         switch (direction) 
         {
-        case 'N':
-            direction = 'W';
-            break;
-        case 'S':
-            direction = 'E';
-            break;
-        case 'E':
-            direction = 'N';
-            break;
-        case 'W':
-            direction = 'S';
-            break;
-        // Up and Down directions remain the same during left/right rotation
+            case 'N':
+                direction = 'W';
+                break;
+            case 'S':
+                direction = 'E';
+                break;
+            case 'E':
+                direction = 'N';
+                break;
+            case 'W':
+                direction = 'S';
+                break;
+            // Up and Down directions remain the same during left/right rotation
         }
     }
 
+    // implementation of rotate right function
     void rotateRight() 
     {
-        // Implement right rotation
         switch (direction) 
         {
-        case 'N':
-            direction = 'E';
-            break;
-        case 'S':
-            direction = 'W';
-            break;
-        case 'E':
-            direction = 'S';
-            break;
-        case 'W':
-            direction = 'N';
-            break;
-        // Up and Down directions remain the same during left/right rotation
-        
+            case 'N':
+                direction = 'E';
+                break;
+            case 'S':
+                direction = 'W';
+                break;
+            case 'E':
+                direction = 'S';
+                break;
+            case 'W':
+                direction = 'N';
+                break;
+            // Up and Down directions remain the same during left/right rotation
         }
     }
 
+    // implementation of rotate up function
     void rotateUp() 
     {
         if (direction == 'N' || direction == 'S') 
@@ -137,6 +147,7 @@ public:
         }
     }
 
+    // implementation of rotate down function
     void rotateDown() 
     {
         if (direction == 'N' || direction == 'S') 
@@ -157,55 +168,68 @@ public:
         }
     }
 
-    tuple<int, int, int, char> getCurrentState()
+    tuple<int, int, int, char> getCurrentState() 
     {
-        return make_tuple(positionX, positionY, positionZ, direction);
+        return std::make_tuple(positionX, positionY, positionZ, direction);
     }
-
-
 };
 
-int main()
-{
-    LunarCraft craft(0, 0, 0, 'N');  // Initialize with starting position and direction
-
-    vector<char> commands = {'f', 'r', 'u', 'b', 'l','d'};
-
-    for (char command : commands) 
+int main() {
+    vector<tuple<string,vector<char>,tuple<int, int, int, char>>> testCases =
     {
-        if (command == 'f') 
-        {
-            craft.moveForward();
-        } 
-        else if (command == 'b') 
-        {
-            craft.moveBackward();
-        } 
-        else if (command == 'r') 
-        {
-            craft.rotateRight();
-        } 
-        else if (command == 'l') 
-        {
-            craft.rotateLeft();
-        } 
-        else if (command == 'u') 
-        {
-            craft.rotateUp();
-        } 
-        else if (command == 'd') 
-        {
-            craft.rotateDown();
+        {"Test 1: Initial Position (0, 0, 0) - Direction N",
+         {'f', 'r', 'u', 'b', 'l'},
+         {0, 0, 0, 'N'}
+         },
+        // Add more test cases as needed
+    };
+
+    for (const auto& testCase : testCases) 
+    {
+        string testName = std::get<0>(testCase);
+        vector<char> commands = std::get<1>(testCase);
+        tuple<int, int, int, char> expectedState = std::get<2>(testCase);
+
+        LunarCraft chandrayaan(get<0>(expectedState),get<1>(expectedState), get<2>(expectedState), std::get<3>(expectedState));
+
+        for (char command : commands) {
+            switch (command) {
+                case 'f':
+                    chandrayaan.moveForward();
+                    break;
+                case 'b':
+                    chandrayaan.moveBackward();
+                    break;
+                case 'r':
+                    chandrayaan.rotateRight();
+                    break;
+                case 'l':
+                    chandrayaan.rotateLeft();
+                    break;
+                case 'u':
+                    chandrayaan.rotateUp();
+                    break;
+                case 'd':
+                    chandrayaan.rotateDown();
+                    break;
+                default:
+                    std::cout << "Invalid command: " << command << std::endl;
+                    break;
+            }
         }
+
+        tuple<int, int, int, char> finalState = chandrayaan.getCurrentState();
+
+        bool testPassed = finalState == expectedState;
+
+        cout << testName << " - " << (testPassed ? "Passed" : "Failed") <<endl;
+        if (!testPassed) {
+            cout << "Expected: Position (" << get<0>(expectedState) << ", " <<get<1>(expectedState) << ", " <<get<2>(expectedState) << ") Direction: " <<get<3>(expectedState) <<endl;
+            cout << "Actual: Position (" <<get<0>(finalState) << ", " <<get<1>(finalState) << ", " <<get<2>(finalState) << ") Direction: " <<get<3>(finalState) <<endl;
+        }
+
+        cout <<endl;
     }
-
-    tuple<int, int, int, char> finalState = craft.getCurrentState();
-    int finalX, finalY, finalZ;
-    char finalDirection;
-    tie(finalX, finalY, finalZ, finalDirection) = finalState;
-
-    cout << "Final Position: (" << finalX << ", " << finalY << ", " << finalZ << ")\n";
-    cout << "Final Direction: " << finalDirection << "\n";
 
     return 0;
 }
